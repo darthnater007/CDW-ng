4e5b2e1684fd361feb05e578ef44cf5d793e8d3a
darthnater007 <nsperk@gmail.com>
2018-05-30 17:14:31 -0400

    created and finished event-create componenet

--- src/app/core/calendar/event-create/event-create.component.ts
+++ src/app/core/calendar/event-create/event-create.component.ts
@@ -9,7 +9,7 @@ import { EventService } from '../../../service/event.service';
   styleUrls: ['./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
-	title: string = "Join Our Group";
+	title: string = "Create An Event";
     confirmPassword: string = '';
     confirmEmail: string = '';
 	resp: any;
@@ -19,7 +19,7 @@ create() {
 	console.log('create a event...');
 	this.eventSvc.create(this.event).subscribe(resp => {
 		this.resp = resp;
-		this.router.navigate(['/ourwriters']);
+		this.router.navigate(['/calendar']);
 		});
 }
 
